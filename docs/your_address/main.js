b9071a9b2ce63a4177970ceff4ce95f53b5e489a
babiron-modder <babiron2014@gmail.com>
2017-11-18 23:11:43 +0900

    first commit

---
+++ docs/your_address/main.js
@@ -0,0 +1,13 @@
+﻿(function(){
+	document.addEventListener("DOMContentLoaded",function(){
+		let ifm = document.getElementById("ifm");
+		let address = document.getElementById("address");
+		
+		ifm.onload = function(){
+			address.innerHTML=ifm.contentWindow.document.getElementById("swml-loc");
+		};
+		
+	});
+	
+	
+})();
