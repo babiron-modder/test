7a302dde80e946b58de419d8cc1c22fd7f7870ea
babiron-modder <babiron2014@gmail.com>
2018-05-26 19:15:58 +0900

    add MDC_test

---
+++ docs/MDC_test/main.js
@@ -0,0 +1,3 @@
+
+let drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
+document.querySelector('.menu').addEventListener('click', () => drawer.open = true);﻿
