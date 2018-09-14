80e6d625db6e3ab8841ce23f2a9558e766050ef5
babiron-modder <babiron2014@gmail.com>
2018-09-14 13:59:03 +0900

    test commit

---
+++ docs/ROMNico/main.js
@@ -0,0 +1,3 @@
+mdc.autoInit();
+let drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
+document.querySelector('.menu').addEventListener('click', () => drawer.open = true);﻿﻿
